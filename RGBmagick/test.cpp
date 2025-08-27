b40e72061a18d28682298f99e2b8a92081188160
DADMIN1 <139256274+DADMIN1@users.noreply.github.com>
2025-08-26 22:57:55 -0800

    LoadImageDirectory ImageGrid linking to MagickCore

--- RGBmagick/test.cpp
+++ RGBmagick/test.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <format>
+#include <vector>
+#include <filesystem>
+
 #include <Magick++.h>
+//#include <magick/MagickCore.h>
 
 
 extern "C" { // libRGBmagick.so exports //
@@ -20,13 +24,45 @@ int EpicFunction(int argc, const char* argv[]) {
 
 const char* GetImageSize(const char imagepath[]) {
 	static std::string _ImageSizeString{"[0x0]"};
-	try { Magick::Image testimage(imagepath);
+	try { Magick::Image testimage(imagepath); testimage.verbose(true);
 		_ImageSizeString = std::format("[{}x{}]", testimage.columns(), testimage.rows());
 	} catch (Magick::ErrorBlob& error){ _ImageSizeString = std::format("[ERROR] {}", error.what());}
 	return _ImageSizeString.c_str();
 }
 
 }// extern "C"
+
+// TODO: prevent ImageMagick from writing temp/cache files to the current directory!!!
+
+// Magick::readImages doesn't accept format-specifiers ("frame%03d.png"); it must be loaded manually
+std::vector<Magick::Image> LoadImageDirectory(std::string directory) {
+	std::vector<Magick::Image> imageList{}; imageList.reserve(1000);
+	// this iterates the files in REVERSE order
+	for(std::filesystem::directory_entry const& file: 
+		std::filesystem::directory_iterator{directory}) {
+		imageList.push_back(Magick::Image(file.path()));
+	}
+	std::cout << std::format("{} loaded: {} images", directory, imageList.size()) << '\n';
+	return imageList;
+}
+
+Magick::Image ImageGrid(int stack_count, std::vector<Magick::Image>& imageList) {
+	std::vector<Magick::Image> stacks{}; stacks.reserve(10);
+	try {
+		for(auto iter {imageList.rbegin()}; iter < imageList.rend();) {
+			std::cout << "next stack: " << iter->fileName() << '\n';
+			Magick::Image& stacked{stacks.emplace_back()};
+			Magick::appendImages(&stacked, iter, iter+stack_count, true); // vertical
+			iter += stack_count;
+		}
+	} catch (Magick::Exception &error) { std::cout << "error: " << error.what() << '\n'; }
+	
+	Magick::Image framegrid{};
+	Magick::appendImages(&framegrid, stacks.begin(), stacks.end(), false); // horizontal
+	std::cout << std::format("stacks: {}x{}", stacks.size(), stack_count) << '\n';
+	std::cout << "final size: " << std::format("[{}x{}]", framegrid.columns(), framegrid.rows()) << '\n';
+	return framegrid;
+}
 
 
 // stripped from library //
@@ -68,6 +104,10 @@ int main(int argc, const char* argv[])
 	std::cout << GetImageSize("testimages/peach.png") << '\n';
 	std::cout << '[' << testimage.columns() << 'x' << testimage.rows() << "]\n";
 	std::cout << std::endl;
+	
+	std::vector<Magick::Image> imageList = LoadImageDirectory("/tmp/RGB_TOPLEVEL/frames/");
+	Magick::Image framegrid = ImageGrid(10, imageList);
+	framegrid.write("/tmp/RGB_TOPLEVEL/output/frame_grid.png");
 	return 0;
 }
 
